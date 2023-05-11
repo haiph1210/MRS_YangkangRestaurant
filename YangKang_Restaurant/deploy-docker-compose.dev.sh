3d56d77711233434fa77c44745f251b1631e2ef6
haiph1210 <haiph12102001@gmail.com>
2023-05-11 16:43:50 +0700

    haiph1210 update docker compose and deploy

---
+++ YangKang_Restaurant/deploy-docker-compose.dev.sh
@@ -0,0 +1,14 @@
+echo "build the project"
+mvn clean install
+
+echo "deploy with redis"
+docker-compose -f docker-compose-tools.yml up -d
+
+echo "deploy with docker compose"
+docker-compose -f docker-compose.dev.yml up -d --force-recreate --build
+
+echo "clean unused images"
+sleep 20
+docker rmi $(docker images -a -q)
+
+echo "completed!"
